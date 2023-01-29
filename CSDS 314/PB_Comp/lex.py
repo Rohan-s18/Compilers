dd31981c19f994e44607e6b20d23141a8add5824
Rohan-s18 <Rohan-s18@github.com>
2023-01-29 16:11:07 -0500

    Added Structure to the Lexer

---
+++ CSDS 314/PB_Comp/lex.py
@@ -0,0 +1,37 @@
+"""
+Author: Rohan Singh
+January 29, 2023
+This Python MOdule contains source code for the Lexer of our Python-based compiler
+"""
+
+# Code for the Lexer class
+class Lexer:
+
+    # init function for the Lexer clsas
+    def __init__(self,input):
+        pass
+
+    # Function to process the next character
+    def nextChar(self):
+        pass
+    
+    # Function to return the lookahead character
+    def peek(self):
+        pass
+
+    # Invalid token error message
+    def abort(self, message):
+        pass
+
+    # Function to skip whitespace
+    def skipWhitespace(self):
+        pass
+
+    # Function to skip a comment
+    def skipComment(self):
+        pass
+    
+    # Function that returns the next character
+    def getToken(self):
+        pass
+
