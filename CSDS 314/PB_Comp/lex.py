ebfc3930bfe884e7c1da4b5fd8b03d2e3e201d1a
Rohan-s18 <Rohan-s18@github.com>
2023-01-29 16:26:51 -0500

    Added functional to the Lexer

--- CSDS 314/PB_Comp/lex.py
+++ CSDS 314/PB_Comp/lex.py
@@ -9,15 +9,28 @@ class Lexer:
 
     # init function for the Lexer clsas
     def __init__(self,input):
-        pass
+        
+        #Getting the source 
+        self.source = input + '\n' 
+
+        #Initializing the instance fields
+        self.curChar = ''
+        self.curPos = -1
+        self.nextChar()
 
     # Function to process the next character
     def nextChar(self):
-        pass
+        self.curPos += 1                                #Moving curPos to the next one
+        if self.curPos >= len(self.source):
+            self.curChar = '\0'                         #EOF
+        else:
+            self.curChar = self.source[self.curPos]     
     
     # Function to return the lookahead character
     def peek(self):
-        pass
+        if self.curPos + 1 >= len(self.source):
+            return '\0'                                 #EOF
+        return self.source[self.curPos+1]
 
     # Invalid token error message
     def abort(self, message):
