5d1834f26363975f551983f93440675e1982fc24
Rohan Singh <rohan.b.singh54@gmail.com>
2023-01-29 17:36:08 -0500

    Added Enum for Token Type

--- CSDS 314/PB_Comp/lex.py
+++ CSDS 314/PB_Comp/lex.py
@@ -76,3 +76,38 @@ class Token:
     def __init__(self, tokenText, tokenKind):
         self.text = tokenText   # The token's actual text. Used for identifiers, strings, and numbers.
         self.kind = tokenKind   # The TokenType that this token is classified as.
+
+
+# Enum for the type of token for token classification
+class TokenType(enum.Enum):
+	EOF = -1
+	NEWLINE = 0
+	NUMBER = 1
+	IDENT = 2
+	STRING = 3
+	
+    # Keywords.
+	LABEL = 101
+	GOTO = 102
+	PRINT = 103
+	INPUT = 104
+	LET = 105
+	IF = 106
+	THEN = 107
+	ENDIF = 108
+	WHILE = 109
+	REPEAT = 110
+	ENDWHILE = 111
+	
+    # Operators.
+	EQ = 201  
+	PLUS = 202
+	MINUS = 203
+	ASTERISK = 204
+	SLASH = 205
+	EQEQ = 206
+	NOTEQ = 207
+	LT = 208
+	LTEQ = 209
+	GT = 210
+	GTEQ = 211
