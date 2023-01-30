f60d53790cc23c44cf2c52c24238eaa875f04a45
Rohan Singh <rohan.b.singh54@gmail.com>
2023-01-29 20:08:42 -0500

    Added Tester code for the parser

---
+++ CSDS 314/PB_Comp/ParseTest.py
@@ -0,0 +1,22 @@
+from lex import *
+from parse import *
+import sys
+
+def main():
+    print("Teeny Tiny Compiler")
+
+    if len(sys.argv) != 2:
+        sys.exit("Error: Compiler needs source file as argument.")
+    with open(sys.argv[1], 'r') as inputFile:
+        input = inputFile.read()
+
+    # Initialize the lexer and parser.
+    lexer = Lexer(input)
+    parser = Parser(lexer)
+
+    parser.program() # Start the parser.
+    print("Parsing completed.")
+
+
+if __name__ == "__main__":
+    main()
